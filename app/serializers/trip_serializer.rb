090319f1832ba1fb0459af2eb507be6b4fed3d76
irosen419 <irosen419@gmail.com>
2020-10-08 21:48:56 -0400

    normalized trip date that comes out of trii serializer

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,5 +1,9 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :destination, :continent, :travel_date, :my_encounters
+  attributes :id, :destination, :continent, :date, :my_encounters
+
+  def date
+    object.travel_date.strftime("%A, %B %d, %Y")
+  end
 
   def my_encounters
     object.encounters.map{|encounter| EncounterSerializer.new(encounter)}
