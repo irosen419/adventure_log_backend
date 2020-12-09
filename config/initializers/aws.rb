6508f670ee155ff6d8c67ccfe6ccdbf77d99f7cf
irosen419 <irosen419@gmail.com>
2020-12-09 15:52:07 -0500

    fixed aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,7 +2,7 @@ require 'aws-sdk-s3'
 
 Aws.config.update({
     region: 'us-east-1',
-    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV ['AWS_SECRET_ACCESS_KEY'])
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 })
 
 S3_BUCKET   = Aws::S3::Resource.new.bucket(ENV['AWS_BUCKET'])
